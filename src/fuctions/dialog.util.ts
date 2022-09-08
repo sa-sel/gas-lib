cf5f4055666752e49d3f767a5d286c4efc83bbe2
Lucas Viana <oviana@outlook.com>
2022-09-07 21:57:54 -0300

    Create dialog function to read user input

--- src/fuctions/dialog.util.ts
+++ src/fuctions/dialog.util.ts
@@ -16,3 +16,26 @@ export const confirm = (dialog: DialogModel, fn: () => void) => {
     }
   }
 };
+
+export const input = (
+  dialog: DialogModel,
+  validate = (userInput: string) => !!userInput,
+  validationErrorMsg = (userInput: string) => `O input "${userInput}" é inválido.`,
+): string | null => {
+  let response: GoogleAppsScript.Base.PromptResponse;
+  let inputValue: string;
+  const body = () => dialog.body + (inputValue === undefined ? '' : `\n\n${validationErrorMsg(inputValue)}`);
+  const ui = GS.ui();
+
+  do {
+    response = ui.prompt(dialog.title, body(), ui.ButtonSet.OK_CANCEL);
+
+    if (response.getSelectedButton() === ui.Button.CANCEL) {
+      GS.ss.toast(DialogMessage.UserCanceled, DialogTitle.Aborted);
+
+      return null;
+    }
+  } while (!validate((inputValue = response.getResponseText())));
+
+  return inputValue;
+};
