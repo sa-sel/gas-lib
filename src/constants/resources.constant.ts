2105237a6b9c645c3e1a3d33341b9d26a0ea4a41
Lucas Viana <oviana@outlook.com>
2023-04-15 18:04:14 -0300

    chore(resources): Save WhatsApp API

--- src/constants/resources.constant.ts
+++ src/constants/resources.constant.ts
@@ -1,4 +1,5 @@
 export const enum ResourceUrl {
   RoundLogo = 'https://i.imgur.com/c38tPYk.png',
   HackermanIcon = 'https://i.imgur.com/G7FpnnQ.png',
+  WhatsAppApi = 'https://api.whatsapp.com/send/?phone={{phone}}',
 }
