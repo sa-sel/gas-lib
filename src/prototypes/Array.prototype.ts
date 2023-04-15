69852de2c2a03990d76d47c9a27ca184cdb5735c
Lucas Viana <oviana@outlook.com>
2023-04-15 14:52:39 -0300

    feat(array): Create Array.prototype.pushIf

--- src/prototypes/Array.prototype.ts
+++ src/prototypes/Array.prototype.ts
@@ -4,12 +4,28 @@ declare global {
   export interface Array<T> {
     /**
      * Split an array into multiple ones by a separator (or function result). Works similarly to `String.prototype.split`.
+     *
+     * @param sep separator element or function
+     * @returns split array
      * @example [1, 2, 1, 2, 3, 4, 1, 1, 6].split(1) -> [[], [2], [2, 3, 4], [], [6]]
      */
     split(sep: T | ((o: T) => boolean)): T[][];
+
+    /**
+     * Appends new elements to the end of an array
+     *
+     * @param cond if elements should be appended
+     * @param items new elements to add to the array
+     * @returns new length of the array
+     */
+    pushIf(cond: any, ...items: T[]): number;
   }
 }
 
 Array.prototype.split = function <T>(sep: T | ((o: T) => boolean)): T[][] {
   return split(this, sep);
+};
+
+Array.prototype.pushIf = function <T>(cond: any, ...items: T[]): number {
+  return cond ? (this as T[]).push(...items) : (this as T[]).length;
 };
