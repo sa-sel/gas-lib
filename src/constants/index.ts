49073aaf08acc8074e2057f5cab4409a8f33ed4b
Lucas Viana <oviana@outlook.com>
2023-04-20 23:00:27 -0300

    feat(template): Add variable enums

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,4 @@ export * from './dialog.constant';
 export * from './emails.constant';
 export * from './globals.constant';
 export * from './resources.constant';
+export * from './template.constant';
---
+++ src/constants/template.constant.ts
@@ -0,0 +1,23 @@
+export const enum MeetingVariable {
+  MeetingType = '{{meetingType}}',
+  MeetingStart = '{{meetingStart}}',
+  MeetingEnd = '{{meetingEnd}}',
+  MeetingDate = '{{meetingDate}}',
+  MeetingDateSort = '{{meetingDateSort}}',
+}
+
+export const enum ProjectVariable {
+  Department = '{{projectDepartment}}',
+  FullDepartment = '{{projectFullDepartment}}',
+  Edition = '{{projectEdition}}',
+  Manager = '{{projectManager}}',
+  ManagerEmail = '{{projectManagerEmail}}',
+  Director = '{{projectDirector}}',
+  DirectorEmail = '{{projectDirectorEmail}}',
+  Name = '{{projectName}}',
+  Start = '{{projectStart}}',
+  NumMembers = '{{projectNumMember}}',
+  Members = '{{projectMembers}}',
+  MembersHtmlList = '{{projectMembersHtmlList}}',
+  FolderUrl = '{{projectFolderUrl}}',
+}
